235e3d2fc5ed976a769fae5af69667d5d0854199
anasommer <68922341+anasommer@users.noreply.github.com>
2024-07-15 10:40:07 +0200

    base tracker, meal & workout classes

--- js/app.js
+++ js/app.js
@@ -5,4 +5,38 @@ class CalorieTracker {
     this._meals = [];
     this._workouts = [];
   }
+
+  addMeal(meal) {
+    this._meals.push(meal);
+    this._totalCalories += meal.calories;
+  }
+
+  addWorkout(workout) {
+    this._workouts.push(workout);
+    this._totalCalories -= workout.calories;
+  }
 }
+
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal('breakfast', 400);
+tracker.addMeal(breakfast);
+
+const run = new Workout('run', 300);
+tracker.addWorkout(run);
